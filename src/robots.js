fe8a5fc46ea0eec650777a48fb4c0d5251c430db
FredOo3 <55452773+FredOo3@users.noreply.github.com>
2020-12-23 12:15:40 +0100

    commit du projet final

---
+++ src/robots.js
@@ -0,0 +1,68 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Fabienne Quivron',
+        username: 'Fabi',
+        email: 'fabienne.reil@orange.fr'
+    },
+    {
+        id: 2,
+        name: 'Frédéric Quivron',
+        username: 'FredOo°',
+        email: 'fquivron@orange.fr'
+    },
+    {
+        id: 3,
+        name: 'Martin Quivron',
+        username: 'Martix',
+        email: 'martin.quivron@orange.fr'
+    },
+    {
+        id: 4,
+        name: 'Sophie Quivron',
+        username: 'Billy',
+        email: 'sophie.quivron@orange.fr'
+    },
+    {
+        id: 5,
+        name: 'René Quivron',
+        username: 'ReniX',
+        email: 'papy.quivron@orange.fr'
+    },
+    {
+        id: 6,
+        name: 'Stéphane Quivron',
+        username: 'Steph',
+        email: 'steph.quivron@free.fr'
+    },
+    {
+        id: 7,
+        name: 'Delphine Quivron',
+        username: 'Delph',
+        email: 'delph.quivron@free.fr'
+    },
+    {
+        id: 8,
+        name: 'Corentin Quivron',
+        username: 'Coco',
+        email: 'Corentin.quivron@free.fr'
+    },
+    {
+        id: 9,
+        name: 'Margaux Quivron',
+        username: 'Mago',
+        email: 'Margaux.quivron@free.fr'
+    },
+    {
+        id: 10,
+        name: 'Christian Reil',
+        username: 'Cricri',
+        email: 'christian.reil@sfr.fr'
+    },
+    {
+        id: 11,
+        name: 'Brigitte Céard ',
+        username: 'Bibi',
+        email: 'brigitte.céard@sft.fr'
+    },
+]
